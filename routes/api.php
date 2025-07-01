d97e4154bf2c2c17cb0235513560aa9cd18e7dca
dba18714 <dba18714@gmail.com>
2025-07-01 23:01:21 +0800

    在api.php中添加TelegramController的webhook和setWebhook路由。

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelegramController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-}); 
+});
+
+Route::post('/telegram/webhook', [TelegramController::class, 'webhook']);
+Route::get('/telegram/set-webhook', [TelegramController::class, 'setWebhook']); 
